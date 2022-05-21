2b8a5ecb5b39bf10f72d3ee1ea2adb1531f14db1
hanmo <hanmo@git.com>
2022-05-21 12:04:07 +0800

    master2 commit5

--- hello.c
+++ hello.c
@@ -15,8 +15,6 @@ int main()
     scanf("%d %d", &a, &b);
     c = add(a, b);
     c = add(a, c);
-    c = add(a, b); // a + b
-    c = add(a, c); // a + b + a
     printf("sum is %d", c);
     return 0;
 }

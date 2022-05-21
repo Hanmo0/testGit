85a4aa628d2b485abc3335d75ee41a2407a4f116
hanmo <hanmo@git.com>
2022-05-21 10:59:51 +0800

    modify helloc.c, first commit

--- hello.c
+++ hello.c
@@ -1,8 +1,18 @@
 # include <stdio.h>
 
+int add(int a, int b)
+{
+    int sum;
+    sum = a + b;
+    return sum;
+}
 
 int main()
 {
-    printf("hello world\r\n");
+    int a = 1, b;
+
+    printf("input two numbers:\r\n");
+    scanf("%d %d", &a, &b);
+    printf("sum is %d", add(a, b));
     return 0;
 }
